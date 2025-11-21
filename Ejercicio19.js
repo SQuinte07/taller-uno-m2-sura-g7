b14f60fe19d383c9f46d91dfdc72d9eeda6d3e9d
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:28:24 -0500

    Ej19

---
+++ Ejercicio19.js
@@ -0,0 +1,7 @@
+let tablero = ["Inicio", null, null, null, null, "Fin"];
+
+console.log("Tablero inicial:", tablero);
+
+tablero.fill("Casilla Vacía", 1, 5);
+
+console.log("Tablero final:", tablero);
