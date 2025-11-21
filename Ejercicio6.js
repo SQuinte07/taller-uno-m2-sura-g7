3b7c2b345819ba5746dc38a05a8e4c283feacea2
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:11:57 -0500

    Ej6

---
+++ Ejercicio6.js
@@ -0,0 +1,10 @@
+let mazo = ["As", "Rey", "Reina", "Jota"];
+
+console.log("Mazo inicial:", mazo);
+
+mazo.reverse();
+console.log("Después de barajar:", mazo);
+
+let cartaSacada = mazo.shift();
+console.log("Carta sacada:", cartaSacada);
+console.log("Mazo después de sacar una carta:", mazo);
