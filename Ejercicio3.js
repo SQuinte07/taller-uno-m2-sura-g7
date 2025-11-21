081b4844816734e9b8fd9113ca2708300b0e5b1c
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:06:28 -0500

    Ej3

---
+++ Ejercicio3.js
@@ -0,0 +1,9 @@
+let historial = ["google.com", "youtube.com", "facebook.com"];
+
+console.log("Historial inicial:", historial);
+
+let deshacer1 = historial.pop();
+console.log("Historial ahora:", historial);
+
+let deshacer2 = historial.pop();
+console.log("Historial final:", historial);
