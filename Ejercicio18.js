a4a542c2985a166c52c2d704a1f6517744da0e88
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:25:39 -0500

    Ej18

---
+++ Ejercicio18.js
@@ -0,0 +1,11 @@
+let invitados = ["Carlos", "Beatriz", "David", "Ana"];
+
+console.log("Lista inicial:", invitados);
+
+invitados.sort();
+
+console.log("Lista ordenada:", invitados);
+
+invitados.splice(1, 1);
+
+console.log("Lista final:", invitados);
