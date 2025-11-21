1cd95c493743ed503aba0273080e518e0e5484dc
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:19:41 -0500

    Ej13

---
+++ Ejercicio13.js
@@ -0,0 +1,11 @@
+let receta = ["Paso 2: Mezclar ingredientes", "Paso 3: Hornear"];
+
+console.log("Receta inicial:", receta);
+
+receta.unshift("Paso 1: Pre-calentar horno");
+
+console.log("Después de añadir el Paso 1:", receta);
+
+receta.push("Paso 4: Servir");
+
+console.log("Después de añadir el Paso 4:", receta);
