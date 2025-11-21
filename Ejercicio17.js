f164d1f4333061dac4d130a95bf9cecbd7e859d3
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:23:44 -0500

    Ej17

---
+++ Ejercicio17.js
@@ -0,0 +1,11 @@
+let feed = ["Foto de Ana", "Video de Juan", "Noticia de Empresa"];
+
+console.log("Feed inicial:", feed);
+
+feed.unshift("Alerta de Sistema");
+
+console.log("Después de añadir la alerta urgente:", feed);
+
+feed.pop();
+
+console.log("Después de eliminar la publicación antigua:", feed);
