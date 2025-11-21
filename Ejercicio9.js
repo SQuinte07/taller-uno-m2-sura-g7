07f93faa3822512416cefaf835ecfe4a9467c01d
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:15:49 -0500

    Ej9

---
+++ Ejercicio9.js
@@ -0,0 +1,7 @@
+let estudiantes = ["Zapata", "Alvarez", "Gomez", "Bernal"];
+
+console.log("Lista inicial de estudiantes:", estudiantes);
+
+estudiantes.sort();
+
+console.log("Lista ordenada alfabéticamente:", estudiantes);
