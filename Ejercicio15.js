51be8648b9bf25c659681a4a192f20f29fb985ad
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:22:26 -0500

    Ej15

---
+++ Ejercicio15.js
@@ -0,0 +1,9 @@
+let equipo = ["Ana (Líder)", "Juan (Dev)", "Maria (Diseñadora)"];
+
+console.log("Equipo inicial:", equipo);
+
+equipo.splice(1, 1);
+
+console.log("Después de que Juan renuncia:", equipo);
+
+console.log("Después de contratar nuevos desarrolladores:", equipo);
