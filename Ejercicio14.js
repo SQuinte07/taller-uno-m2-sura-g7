d1688f1418289eb7194ea79d7b49a8154b330a07
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:21:05 -0500

    Ej14

---
+++ Ejercicio14.js
@@ -0,0 +1,11 @@
+let palabras = ["uno", "dos", "cuatro"];
+
+console.log("Lista inicial:", palabras);
+
+palabras.reverse();
+
+console.log("Después de invertir:", palabras);
+
+palabras.splice(0, 1, "tres");
+
+console.log("Después de reemplazar cuatro por tres:", palabras);
