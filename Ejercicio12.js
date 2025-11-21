46718974d9a4d87e96441c64e89aa1db3214b616
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:18:58 -0500

    Ej12

---
+++ Ejercicio12.js
@@ -0,0 +1,15 @@
+let commits = ["v1.0", "v1.1"];
+
+console.log("Commits iniciales:", commits);
+
+commits.push("v1.2");
+
+console.log("Después de añadir v1.2:", commits);
+
+commits.pop();
+
+console.log("Después de deshacer v1.2:", commits);
+
+commits.push("v1.2-corregido");
+
+console.log("Después de añadir v1.2-corregido:", commits);
