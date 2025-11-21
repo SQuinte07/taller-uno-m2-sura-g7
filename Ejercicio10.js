c39dba18488796915d9080db5701980eb4b28f81
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:16:23 -0500

    Ej10

---
+++ Ejercicio10.js
@@ -0,0 +1,6 @@
+let puntuaciones = [100, 85, 92, 78];
+
+console.log("Puntuaciones iniciales:", puntuaciones);
+
+puntuaciones.fill(0);
+console.log("Puntuaciones después del reset:", puntuaciones);
